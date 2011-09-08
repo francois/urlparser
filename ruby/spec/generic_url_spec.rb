1ca6b497e63438715e156ac10614260fadbb05ae
François Beausoleil <francois@teksol.info>
2011-09-08 16:47:26 -0400

    More failing specs

--- ruby/spec/generic_url_spec.rb
+++ ruby/spec/generic_url_spec.rb
@@ -30,5 +30,34 @@ describe "Parsing the generic URL 'tcp://fbeausoleil:thepassword@c.com:3391/def'
   it "should recognize the '/def' path" do
     url.path.must_equal "/def"
   end
+end
 
+describe "Parsing the generic URL 'udp://127.0.0.1:1234'" do
+  def url
+    @url ||= UrlParser.parse("udp://127.0.0.1:1234")
+  end
+
+  it "should recognize the 'udp' scheme" do
+    url.scheme.must_equal "udp"
+  end
+
+  it "should NOT recognize a user" do
+    url.user.must_be_nil
+  end
+
+  it "should NOT recognize a password" do
+    url.password.must_be_nil
+  end
+
+  it "should recognize a numerical host" do
+    url.host.must_equal "127.0.0.1"
+  end
+
+  it "should recognize a port" do
+    url.port.must_equal 1234
+  end
+
+  it "should NOT recognize a path" do
+    url.path.must_be_nil
+  end
 end
